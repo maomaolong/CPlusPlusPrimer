f5b517dea8f9f9207d08452076439effb26325ba
maomaolong <562522970@qq.com>
2014-06-24 20:15:29 +0800

    add section12
    add section12 project

---
+++ CCPrimer5/Section12/Section12.cpp
@@ -0,0 +1,13 @@
+// Section12.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"hello world!"<<endl;
+	getchar();
+	return 0;
+}
+
